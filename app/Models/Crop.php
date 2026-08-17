450698e9dc086b65696581053aebda99f82f669f
nuharizvi03 <aayshanuha633@gmail.com>
2026-08-17 16:13:28 +0530

    Add crop management module

---
+++ app/Models/Crop.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Plot;
+
+class Crop extends Model
+{
+    protected $fillable = [
+        'plot_id',
+        'crop_name',
+        'variety',
+        'planting_date',
+        'expected_harvest_date',
+        'season',
+        'status',
+        'notes',
+    ];
+
+    protected $casts = [
+        'planting_date' => 'date',
+        'expected_harvest_date' => 'date',
+    ];
+
+    /**
+     * A crop belongs to a plot.
+     */
+    public function plot(): BelongsTo
+    {
+        return $this->belongsTo(Plot::class);
+    }
+}
